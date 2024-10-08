c7eb5cd51513c60ee074b1e2f77ab7c8258628b5
sgimenelan <sgimenelan@educacion.navarra.es>
2024-10-08 09:47:12 +0200

    Create Ejercicio09.py

---
+++ Ejercicio09.py
@@ -0,0 +1,12 @@
+a = input("¿Con que tipo de interes quieres invertir? (simple o compuesto) ")
+a = a.lower()
+capital = int(input("Cuanto es la capital que quieres invertir "))
+interes = int(input("Cuanto es el interes anual "))
+interes = (interes / 100)
+años = int(input("¿Cuantos años quieres invertir? "))
+if a == "simple":
+    b = (capital * (1 + interes * años))
+    print ("Tu capital final es ", round (b, 2),"€")
+if a == "compuesto":
+    b = (capital * (1 + interes)** años)
+    print ("Tu capital final es ", round (b, 2),"€")
